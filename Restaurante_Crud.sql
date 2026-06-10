420027799f0100c81df77971e6dbb291640f68fc
mariagracia22 <maria.calderon.zevallos@ucsp.edu.pe>
2026-06-10 07:39:20 -0500

    feat crud pagos

--- Restaurante_Crud.sql
+++ Restaurante_Crud.sql
@@ -1,1 +1,12 @@
 
+--Pagos
+INSERT INTO PAGO (monto, metodo, fcha_pago, estado, id_pedido) 
+VALUES (15000, 'tarjeta', CURRENT_DATE, 'completado', 5);
+
+INSERT INTO FACTURA (numero, fecha_emision, monto_total, id_pago) 
+VALUES ('FAC-001', CURRENT_DATE, 15000, LASTVAL());
+
+UPDATE PEDIDO SET estado = 'Pagado' WHERE id_pedido = 5;
+
+UPDATE PAGO SET estado = 'verificado' WHERE id_pedido = 5;
+
