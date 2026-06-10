46eb8d55ecb3a7857ac2f92c8d1a97413d92a8a5
mariagracia22 <maria.calderon.zevallos@ucsp.edu.pe>
2026-06-10 07:35:23 -0500

    fest

--- Restaurante_Update.sql
+++ Restaurante_Update.sql
@@ -1,3 +1,7 @@
 UPDATE SUCURSAL 
 SET nombre='Sucursal Habanas',direccion='Av.Bellavista 432'
 WHERE ciudad='Arequipa';
+
+UPDATE PEDIDO
+SET id_empleado= 2, id_mesa= 4, estado='Entregado', total=150
+WHERE id_cliente=3;
