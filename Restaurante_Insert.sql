956419dbbb8f0f95936167531351f9511e55d0d7
jadevillavicencio <jade.villavicencio@ucsp.edu.pe>
2026-06-10 06:57:25 -0500

    feat agregar sucursal

--- Restaurante_Insert.sql
+++ Restaurante_Insert.sql
@@ -1,1 +1,7 @@
-
+INSERT INTO SUCURSAL(nombre,dirección,telefono,ciudad)
+VALUES
+    ('Sucursal Centro', 'Av. Principal 123', '555-0101', 'Lima'),
+    ('Sucursal Norte', 'Calle Norte 456', '555-0102', 'Lima'),
+    ('Sucursal Sur', 'Av. Del Sur 789', '555-0103', 'Arequipa'),
+    ('Sucursal Este', 'Jr. Este 321', '555-0104', 'Cusco'),
+    ('Sucursal Oeste', 'Av. Oeste 654', '555-0105', 'Trujillo');
