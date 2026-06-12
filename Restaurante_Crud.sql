258bd8ea9c959b1ca1413650d61ed1a560a3ab96
mariagracia22 <maria.calderon.zevallos@ucsp.edu.pe>
2026-06-11 20:56:21 -0500

    fix values pago

--- Restaurante_Crud.sql
+++ Restaurante_Crud.sql
@@ -1,7 +1,7 @@
 
 --Pagos
 INSERT INTO PAGO (monto, metodo, fcha_pago, estado, id_pedido) 
-VALUES (15000, 'tarjeta', CURRENT_DATE, 'completado', 5);
+VALUES (15000, 'tarjeta debito', CURRENT_DATE, 'completado', 5);
 
 INSERT INTO FACTURA (numero, fecha_emision, monto_total, id_pago) 
 VALUES ('FAC-001', CURRENT_DATE, 15000, LASTVAL());
