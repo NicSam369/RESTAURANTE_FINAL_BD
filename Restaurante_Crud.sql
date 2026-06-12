3d403aa958be77861ed0d5e0f3003ba9ac510ac3
NicSam369 <samantha.huayapa@ucsp.edu.pe>
2026-06-11 19:58:47 -0500

    feat: Crud - compra a proveedor

--- Restaurante_Crud.sql
+++ Restaurante_Crud.sql
@@ -10,3 +10,21 @@ UPDATE PEDIDO SET estado = 'Pagado' WHERE id_pedido = 5;
 
 UPDATE PAGO SET estado = 'verificado' WHERE id_pedido = 5;
 
+--Compra a proveedor
+INSERT INTO COMPRA (fecha, total, estado, id_scrsal, id_prvdor)
+VALUES (CURRENT_DATE, 0, 'recibido', 1, 1);
+
+INSERT INTO DETALLE_COMPRA (cantidad, subtotal, precio_unitario, id_producto, id_compra)
+VALUES (36, 36*800, 800, 10, LASTVAL());
+
+UPDATE PRODUCTO
+SET stock = stock + 36
+WHERE id_producto = 10;
+
+UPDATE COMPRA
+SET total = (
+  SELECT SUM(subtotal)
+  FROM DETALLE_COMPRA
+  WHERE id_compra = LASTVAL()
+) 
+WHERE id_compra = LASTVAL();
