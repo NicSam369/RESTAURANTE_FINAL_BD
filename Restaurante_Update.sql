5dd799883041ed103947bdbedc5d5abf4646dd13
NicSam369 <samantha.huayapa@ucsp.edu.pe>
2026-06-11 19:40:55 -0500

    fix: update proveedor

--- Restaurante_Update.sql
+++ Restaurante_Update.sql
@@ -5,3 +5,7 @@ WHERE ciudad='Arequipa';
 UPDATE PEDIDO
 SET id_empleado= 2, id_mesa= 4, estado='Entregado', total=150
 WHERE id_cliente=4;
+
+UPDATE PROVEEDOR
+SET nombre='Tyler', ruc='20552103816', telefono='598005744', email='Tlr_937@gmail.com'
+WHERE id_scrsal='4';
