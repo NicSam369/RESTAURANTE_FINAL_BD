a2b888ecb7ba07cc7ad07ceceaa5a3e203889cfc
Ricardo Velasquez <93832900+rickyvels@users.noreply.github.com>
2026-06-20 18:38:50 -0500

    Update Restaurante_Transacciones.sql

--- Restaurante_Transacciones.sql
+++ Restaurante_Transacciones.sql
@@ -1,1 +1,8 @@
+BEGIN;
+INSERT INTO PAGO (monto, metodo, fcha_pago, estado, id_pedido)
+VALUES (15000, 'tarjeta debito', CURRENT_DATE, 'completado', 5);
+INSERT INTO FACTURA (numero, fecha_emision, monto_total, id_pago)
+VALUES ('FAC-001', CURRENT_DATE, 15000, 1);
+UPDATE PEDIDO SET estado = 'Pagado' WHERE id_pedido = 5;
+COMMIT;
 
