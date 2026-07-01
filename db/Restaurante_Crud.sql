0c1c30457eaf5f152e9f5a1a2378aebb8df20264
jadevillavicencio <jade.villavicencio@ucsp.edu.pe>
2026-06-30 23:02:00 -0500

    fix: correcciones en orden

--- db/Restaurante_Crud.sql
+++ db/Restaurante_Crud.sql
@@ -3,28 +3,23 @@
 INSERT INTO PAGO (monto, metodo, fcha_pago, estado, id_pedido) 
 VALUES (15000, 'tarjeta debito', CURRENT_DATE, 'pendiente', 5);
 
-INSERT INTO FACTURA (numero, fecha_emision, monto_total, id_pago) 
-VALUES ('FAC-001', CURRENT_DATE, 15000, LASTVAL());
+INSERT INTO FACTURA (numero, fecha_emision, monto_total, id_pago)
+VALUES ('FAC-C01', CURRENT_DATE, 150.00, currval('pago_id_pago_seq'));
 
 UPDATE PEDIDO SET estado = 'Pagado' WHERE id_pedido = 5;
 
-UPDATE PAGO SET estado = 'completado' WHERE id_pedido = 5;
 
 --Compra a proveedor
 INSERT INTO COMPRA (fecha, total, estado, id_scrsal, id_prvdor)
 VALUES (CURRENT_DATE, 0, 'recibido', 1, 1);
 
 INSERT INTO DETALLE_COMPRA (cantidad, subtotal, precio_unitario, id_producto, id_compra)
-VALUES (36, 36*800, 800, 10, LASTVAL());
+VALUES (36, 36 * 8.00, 8.00, 10, currval('compra_id_compra_seq'));
 
-UPDATE PRODUCTO
-SET stock = stock + 36
-WHERE id_producto = 10;
+UPDATE PRODUCTO SET stock = stock + 36 WHERE id_producto = 10;
 
 UPDATE COMPRA
-SET total = (
-  SELECT SUM(subtotal)
-  FROM DETALLE_COMPRA
-  WHERE id_compra = LASTVAL()
-) 
-WHERE id_compra = LASTVAL();
+SET total = (SELECT SUM(subtotal) FROM DETALLE_COMPRA
+             WHERE id_compra = currval('compra_id_compra_seq'))
+WHERE id_compra = currval('compra_id_compra_seq');
+
